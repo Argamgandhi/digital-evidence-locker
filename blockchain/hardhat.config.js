14b230109691c7bb56134bd1fd68d7be0244f341
Argamgandhi <argamgandhi@gmail.com>
2026-03-10 17:34:45 +0530

    Deploy contract to Sepolia testnet

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -4,7 +4,11 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545",
+      url: "http://127.0.0.1:8545"
     },
-  },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/NEdki-doZp6hs2eRSdAn_",
+      accounts: ["d49328a96a9ea2ef1f21379762edb7055116460d0be596a1288e3ddc7e336e25"]
+    }
+  }
 };
